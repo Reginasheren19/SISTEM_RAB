c4c31f9b525507f8ec4f411273594b591c927468
Ekkle03 <esia2003mt@gmail.com>
2025-07-05 18:44:36 +0700

    .

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -14,26 +14,27 @@ $user_role = strtolower($_SESSION['role']);
 // Gunakan switch case untuk mengarahkan ke dashboard yang sesuai
 switch ($user_role) {
     case 'admin':
-        header("Location: dashboard_admin.php");
+        //header("Location: dashboard_admin.php");
+        header("Location: dashboard_admin_m.php");
+
         break;
     
     case 'direktur':
-        header("Location: dashboard_direktur.php");
+        //header("Location: dashboard_direktur.php");
+        header("Location: dashboard_direk_m.php");
         break;
         
     case 'pj proyek':
         // Pastikan nama file ini sesuai dengan yang Anda gunakan
-        header("Location: dashboard_pjproyek.php"); 
+        // header("Location: dashboard_pjproyek.php"); 
+        header("Location: dashboard_pj_m.php"); 
         break;
         
     case 'divisi teknik':
-        header("Location: dashboard_teknik.php");
+        // header("Location: dashboard_teknik.php");
+        header("Location: dashboard_divtek_m.php");
         break;
         
-    // Anda bisa menambahkan case untuk role lain jika ada di masa depan
-    // case 'sekretaris':
-    //     header("Location: dashboard_sekretaris.php");
-    //     break;
         
     default:
         // Sebagai fallback jika role tidak dikenali, arahkan ke halaman login
